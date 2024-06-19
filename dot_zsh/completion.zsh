7736977b3f04ca4776d3155c3e7672990d677b6e
johanweitner@gmail.com <johanweitner@gmail.com>
2024-06-19 22:26:54 +0200

    Remove debug logs

--- dot_zsh/completion.zsh
+++ dot_zsh/completion.zsh
@@ -1,4 +1,4 @@
 [[ -f ~/.zsh/completions/hoard.zsh ]] && source ~/.zsh/completions/hoard.zsh
-echo "Sourced Hoard completions."
+# echo "Sourced Hoard completions."
 [[ -f ~/.zsh/completions/chezmoi.zsh ]] && source ~/.zsh/completions/chezmoi.zsh
-echo "Sourced Chezmoi completions."
+# echo "Sourced Chezmoi completions."
