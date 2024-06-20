efceea9f89e132eba089fb21a58d34b2422e442c
johanweitner@gmail.com <johanweitner@gmail.com>
2024-06-21 01:05:36 +0200

    WiP

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-echo ""
-echo "Setting baseline environment variables..."
-export FULL_NAME="Johan Weitner"
-export PRIMARY_EMAIL="johanweitner@gmail.com"
-export GITHUB_USER="johan-weitner"
-export GITHUB_EMAIL=$PRIMARY_EMAIL
-echo "Done."
-echo "You probably also want to set the secret key for unlocking encrypted assets."
+
+# Ensure Homebrew is installed
+if ! command -v brew &> /dev/null; then
+  echo "Homebrew is not installed. Installing..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Ensure Chezmoi is installed
+if ! command -v chezmoi &> /dev/null; then
+    echo "Chezmoi is not installed. Installing..."
+    brew install chezmoi
+fi
+
+# Decrypt the Age key
+echo "Decrypting Age key..."
+source decrypt-age-key.sh
+
+# Apply Chezmoi configuration
+echo "Applying Chezmoi configuration..."
+chezmoi init
+
+# Now that the Age key is decrypted, re-apply Chezmoi configuration to use the decrypted key
+echo "Re-applying Chezmoi configuration with decrypted Age key..."
+chezmoi apply
