fa1c7fc06d7ba60049e10b3b3f73437065b1d3d1
johanweitner@gmail.com <johanweitner@gmail.com>
2024-06-20 13:17:03 +0200

    Add bootstrap script

---
+++ bin/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo ""
+echo "Setting baseline environment variables..."
+export FULL_NAME="Johan Weitner"
+export PRIMARY_EMAIL="johanweitner@gmail.com"
+export GITHUB_USER="johan-weitner"
+export GITHUB_EMAIL=$PRIMARY_EMAIL
+echo "Done."
+echo "You probably also want to set the secret key for unlocking encrypted assets."
