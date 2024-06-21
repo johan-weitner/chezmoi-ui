34a76275129bf5887d4a8ea65ac22d849640344f
johanweitner@gmail.com <johanweitner@gmail.com>
2024-06-21 22:05:01 +0200

    WiP

--- dot_zsh/plugins.zsh
+++ dot_zsh/plugins.zsh
@@ -3,3 +3,13 @@
 
 #Zoxide
 eval "$(zoxide init zsh)"
+
+# Load Direnv
+eval "$(direnv hook zsh)"
+
+# Hishtory Config:
+export PATH="$PATH:$HOME/.hishtory"
+source $HOME/.hishtory/config.zsh
+
+# Autojump
+ [ -f /opt/homebrew/etc/profile.d/autojump.sh ] && . /opt/homebrew/etc/profile.d/autojump.sh
