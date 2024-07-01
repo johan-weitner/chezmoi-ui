2766439e55675a54d5edca2c5515cf4ec2f25aa7
johanweitner@gmail.com <johanweitner@gmail.com>
2024-07-01 13:51:59 +0200

    Separating UI from Chezmoi itself; removing Chezmoi stuff

--- bin/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo ""
-echo "Starting clean install..."
-#chezmoi init https://$PRIVATE_GITHUB_TOKEN@github.com/johan-weitner/dotfiles.git
-# sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
-export PATH="$HOME/bin:$PATH"
-
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply https://$PRIVATE_GITHUB_TOKEN@github.com/johan-weitner/dotfiles.git
--- dot_zsh/completion.zsh
+++
@@ -1,4 +0,0 @@
-[[ -f ~/.zsh/completions/hoard.zsh ]] && source ~/.zsh/completions/hoard.zsh
-# echo "Sourced Hoard completions."
-[[ -f ~/.zsh/completions/chezmoi.zsh ]] && source ~/.zsh/completions/chezmoi.zsh
-# echo "Sourced Chezmoi completions."
