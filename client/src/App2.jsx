5a482f790591dbdba101046e6d1872d800376189
johanweitner@gmail.com <johanweitner@gmail.com>
2024-07-08 06:09:29 +0200

    WiP

--- client/src/App2.jsx
+++ client/src/App2.jsx
@@ -26,15 +26,15 @@ function App() {
 		});
 	};
 
-	// const useApp = (key) => {
-	// 	return useQuery({
-	// 		queryKey: ["app"],
-	// 		queryFn: async () => {
-	// 			const response = await axios.get(`${BASE_URL}/getApp?key=${key}`);
-	// 			return response.data;
-	// 		},
-	// 	});
-	// };
+	const useApp = (key) => {
+		return useQuery({
+			queryKey: ["app"],
+			queryFn: async () => {
+				const response = await axios.get(`${BASE_URL}/getApp?key=${key}`);
+				return response.data;
+			},
+		});
+	};
 
 	const openApp = async (key) => {
 		const app = await getApp(key);
