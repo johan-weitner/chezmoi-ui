bac2dd8b3dff9843dc1517845ddfa6a20ee7bc2c
johanweitner@gmail.com <johanweitner@gmail.com>
2024-07-28 05:15:46 +0200

    WiP

--- client/src/views/ListView/SearchWidget.jsx
+++ client/src/views/ListView/SearchWidget.jsx
@@ -1,63 +1,89 @@
-import { ActionIcon, Tooltip, rem } from "@mantine/core";
-import { Spotlight, spotlight } from "@mantine/spotlight";
+import { ActionIcon, Text, Tooltip, rem } from "@mantine/core";
 import { IconFileText, IconSearch } from "@tabler/icons-react";
 import { useEffect, useState } from "react";
 import { useClientManager } from "../../core/ClientManager";
 import { rootStore } from "../../store/store";
 import "components/neumorphic.css";
+import { ReactSearchAutocomplete } from "react-search-autocomplete";
+import "./SearchWidget.css";
 
-const actions = [];
 const SearchWidget = (props) => {
+	const [isOpen, setIsOpen] = useState(false);
 	const { setSelectedAppKey } = useClientManager();
 
 	const openApp = (key) => {
+		console.log("Open app", key);
 		setSelectedAppKey(key);
 	};
 
-	useEffect(() => {
-		const appCollection = rootStore.get.appCollection();
-		appCollection?.map((app) => {
-			actions.push({
-				id: app.key,
-				label: app.name,
-				description: app.short,
-				onClick: () => openApp(app.key),
-				leftSection: (
-					<IconFileText
-						style={{ width: rem(24), height: rem(24) }}
-						stroke={1.5}
-					/>
-				),
-			});
-		});
-	}, [rootStore.use.appCollection()]);
+	const handleOnSearch = (string, results) => {
+		// console.log(string, results);
+	};
+
+	const handleOnHover = (result) => {
+		// console.log(result);
+	};
+
+	const handleOnSelect = (item) => {
+		openApp(item.key);
+		setIsOpen(false);
+	};
+
+	const handleOnFocus = () => {
+		console.log("Focused");
+	};
+
+	const formatResult = (item) => {
+		return (
+			<Text
+				style={{ color: "#1769b3", display: "block", textAlign: "left" }}
+				onClick={() => openApp(item.key)}
+				fw="bold"
+			>
+				{item.name}
+			</Text>
+		);
+	};
 
 	return (
 		<ActionIcon.Group>
 			<ActionIcon
-				onClick={spotlight.open}
 				size="xl"
 				style={{ position: "absolute", top: "25px", right: "120px" }}
 				className="neubtn"
+				onClick={() => setIsOpen(!isOpen)}
 			>
 				<Tooltip label="Free text search for apps" position="top">
 					<IconSearch size={24} color="#999" />
 				</Tooltip>
 			</ActionIcon>
-			<Spotlight
-				actions={actions}
-				nothingFound="Nothing found..."
-				highlightQuery
-				searchProps={{
-					leftSection: (
-						<IconSearch
-							style={{ width: rem(20), height: rem(20) }}
-							stroke={1.5}
-						/>
-					),
-					placeholder: "Search apps...",
-				}}
-			/>
+			{isOpen && (
+				<div
+					className="searchWidget"
+					style={{
+						zIndex: "99999",
+					}}
+				>
+					<header className="searchWidgetHeader">
+						<div style={{ width: 400 }}>
+							<ReactSearchAutocomplete
+								items={
+									rootStore.get.appCollection()?.length &&
+									rootStore.get.appCollection()
+								}
+								resultStringKeyName="name"
+								onSearch={handleOnSearch}
+								onHover={handleOnHover}
+								onSelect={handleOnSelect}
+								onFocus={handleOnFocus}
+								autoFocus
+								formatResult={formatResult}
+								style={{ backgroundColor: "#222" }}
+							/>
+						</div>
+					</header>
+				</div>
+			)}
 		</ActionIcon.Group>
 	);
 };
