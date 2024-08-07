765630cfb9e6b89d039d696b993dcb94088b8b24
johanweitner@gmail.com <johanweitner@gmail.com>
2024-08-07 14:19:04 +0200

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -5,10 +5,11 @@ echo "***** Chezmoi UI Setup *****"
 cp client/.env.example client/.env
 cp server/.env.example server/.env
 
+npm i -g pnpm
 pnpm installDeps
 
-# pnpm i -g turbo
+cd server/ && pnpx prisma migrate dev --schema=./prisma/schema.prisma && cd ..
 
 echo "***** Done. *****"
-echo "Run 'start.sh' (or start.ps1 on Windows) to start the app."
+echo "Run 'pnpm start' to start the app."
 echo "Alternatively, run 'docker-compose up' in the root directory."
